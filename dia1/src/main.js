67836405d2ee34946705eeecb1bae1d7e8795abb
Marco <marcomonteiro94@hotmail.com>
2021-08-17 09:42:10 -0300

    Exercício 4 - Adicionar um link para mudar o display do #app no click

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,15 @@ document.querySelector('[data-js=app]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+document.querySelector('#hideApp').addEventListener('click', function () {
+  const divApp = document.querySelector('[data-js=app]').style;
+  const hideAppText = document.querySelector('#hideApp')
+  if (divApp.display === 'none') {
+    divApp.display = "block";
+    hideAppText.innerHTML = `Desaparecer 🐱‍👤`
+  } else {
+    divApp.display = "none";
+    hideAppText.innerHTML = `Reaparecer 🌟`
+  }
+})
